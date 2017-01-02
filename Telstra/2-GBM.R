60d5504bc73c6094c6de525e69895047a5389e60
Socrates <socratesk@users.noreply.github.com>
2017-01-02 00:12:07 -0600

    Code formatted

--- Telstra/2-GBM.R
+++ Telstra/2-GBM.R
@@ -24,59 +24,59 @@ set.seed(220469)
 
 # Load Train dataset and rearrange 'Location' feature
 len <- nchar('location ')
-train 			<- read.csv('train.csv',header=TRUE,stringsAsFactors = F)
+train 		<- read.csv('train.csv',header=TRUE,stringsAsFactors = F)
 train$location 	<- substr(train$location, len + 1, len + 4)
 train$location 	<- as.numeric(gsub(' ', '', train$location))
 
 # Load Test dataset and rearrange 'Location' feature
-test 			<- read.csv('test.csv',header=TRUE,stringsAsFactors = F) # 11171 x 2
+test 		<- read.csv('test.csv',header=TRUE,stringsAsFactors = F) # 11171 x 2
 test$location 	<- substr(test$location, len + 1, len + 4)
 test$location 	<- as.numeric(gsub(' ', '', test$location))
 
 # Load and expand 'Resource Type' feature
-len 					<- nchar('resource_type ')
-resource 				<- read.csv('resource_type.csv', header=TRUE, stringsAsFactors = F)
-resource$value 			<- as.numeric(substr(resource$resource_type, len + 1, len + 2))
+len 			<- nchar('resource_type ')
+resource 		<- read.csv('resource_type.csv', header=TRUE, stringsAsFactors = F)
+resource$value 		<- as.numeric(substr(resource$resource_type, len + 1, len + 2))
 resource$resource_type 	<- gsub(' ', '.', resource$resource_type)
-resource_df 			<- dcast(resource, id ~ resource_type, value.var = "value")
+resource_df 		<- dcast(resource, id ~ resource_type, value.var = "value")
 
 # Replace NAs to -1 in 'Resource Type' feature
 resource_df[is.na(resource_df)] <- 0
 
 # Load and expand 'Event Type' feature
-len 				<- nchar('event_type ')
-event 				<- read.csv('event_type.csv', header=TRUE, stringsAsFactors = F)
-event$value 		<- as.numeric(substr(event$event_type, len + 1, len + 2))
-event$event_type 	<- gsub(' ', '.', event$event_type)
-event_df 			<- dcast(event, id ~ event_type, value.var = "value")
+len 		 <- nchar('event_type ')
+event 		 <- read.csv('event_type.csv', header=TRUE, stringsAsFactors = F)
+event$value 	 <- as.numeric(substr(event$event_type, len + 1, len + 2))
+event$event_type <- gsub(' ', '.', event$event_type)
+event_df 	 <- dcast(event, id ~ event_type, value.var = "value")
 
 # Replace NAs to -1 in 'EventType' feature
 event_df[is.na(event_df)] <- 0
 
 # Load and expand 'Severity Type' feature 
-len 					<- nchar('severity_type ')
-severity 				<- read.csv('severity_type.csv', header=TRUE, stringsAsFactors = F) # 18552 x 2
-severity$value 			<- as.numeric(substr(severity$severity_type, len + 1, len + 2))
+len 			<- nchar('severity_type ')
+severity 		<- read.csv('severity_type.csv', header=TRUE, stringsAsFactors = F) # 18552 x 2
+severity$value 		<- as.numeric(substr(severity$severity_type, len + 1, len + 2))
 severity$severity_type 	<- gsub(' ', '.', severity$severity_type)
-severity_df 			<- dcast(severity, id ~ severity_type, value.var = "value") # 18552 x 6
+severity_df 		<- dcast(severity, id ~ severity_type, value.var = "value") # 18552 x 6
 
 # Replace NAs to -1 in 'EventType' feature
 severity_df[is.na(severity_df)] <- 0
 
 # Merge Train data with 'Resource Type', 'Event Type', 'Severity' data
-train_res 			<- merge(train, resource_df, by="id")
+train_res 		<- merge(train, resource_df, by="id")
 train_res_evn 		<- merge(train_res, event_df, by="id")
-train_res_evn_sev 	<- merge(train_res_evn, severity_df, by="id")
-trainold 			<- train
-train 				<- train_res_evn_sev
+train_res_evn_sev	<- merge(train_res_evn, severity_df, by="id")
+trainold 		<- train
+train 			<- train_res_evn_sev
 rm(train_res_evn_sev)
 
 # Merge Test data with 'Resource Type', 'Event Type', 'Severity' data
-test_res 			<- merge(test, resource_df, by="id")
+test_res 		<- merge(test, resource_df, by="id")
 test_res_evn 		<- merge(test_res, event_df, by="id")
 test_res_evn_sev 	<- merge(test_res_evn, severity_df, by="id")
-testold 			<- test
-test 				<- test_res_evn_sev
+testold 		<- test
+test 			<- test_res_evn_sev
 rm(test_res_evn_sev)
 
 # Remove ID fields
@@ -107,36 +107,38 @@ levels(trainpredictor) <- make.names(levels(factor(trainpredictor)))
 train$fault_severity <- NULL
 
 # Create 'Train Control' object
-traincontrol <- trainControl(method = "repeatedcv"
-				, number 			= 10
-				, repeats 			= 10
-				, verboseIter 		= FALSE
-				, returnResamp		= "all" 
-				, classProbs		= TRUE 
-				#, summaryFunction	= MCLogLoss
-				) 
+traincontrol <- trainControl(method 		= 'repeatedcv'
+			, number 		= 10
+			, repeats 		= 10
+			, verboseIter 		= FALSE
+			, returnResamp		= 'all'
+			, classProbs		= TRUE 
+			#, summaryFunction	= MCLogLoss
+		) 
 
 # Create 'Train Grid' Object
-traingrid <-  expand.grid(interaction.depth = 6 	# c(1, 5, 9),
-                       , n.trees = 60 		#(1:30)*50
-                       , shrinkage = 0.1
-                       , n.minobsinnode = 10) 	#20
+traingrid <-  expand.grid(interaction.depth 	= 6 	# c(1, 5, 9),
+                       , n.trees 		= 60 	# (1:30)*50
+                       , shrinkage 		= 0.1
+                       , n.minobsinnode 	= 10 	# 20
+		)
 
 # Train GBM Model
-gbmModel <- train(x 		= as.data.frame(train)
-				, y 		= trainpredictor
-				, method 	= "gbm"
-				, trControl = traincontrol
-				, tuneGrid 	= traingrid
-				, verbose 	= FALSE)
+gbmModel <- train(x 		= train
+		, y 		= trainpredictor
+		, method 	= 'gbm'
+		, trControl 	= traincontrol
+		, tuneGrid 	= traingrid
+		, verbose 	= FALSE
+	     )
 
 # Predict probabilities for Test dataset
 ypred <- predict(gbmModel, test, type="prob") 
 
 # Convert predicted probabilities to Decimal from Scientific notation
-ypred <- format(ypred, scientific = FALSE)
+ypred <- format(ypred, scientific=FALSE)
 
 # Create Submission file
-colnames(ypred) <- paste("predict_", c(0,1,2), sep="")
-resp 			<- data.frame(id = testid, predMatrix)
+colnames(ypred) <- paste("predict_", c(0, 1, 2), sep="")
+resp 		<- data.frame(id = testid, ypred)
 write.csv(resp, 'Submission-2-GBM.csv', row.names=F, quote = F)
